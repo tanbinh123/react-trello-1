c13e68591656dc1cc3045330cc6f3154ca2d2a07
Mark Pollmann <markpollmann@web.de>
2020-01-03 12:53:28 +0100

    reload on logout

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -4,6 +4,7 @@ export const handleLogout = async () => {
   try {
     await Auth.signOut()
     console.log('signing out!')
+    window.location.reload()
   } catch (error) {
     console.error('error during logout', error)
   }
