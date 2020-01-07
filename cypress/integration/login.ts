4edb172fdb4bb18e47880a09e7dfe988b83d5f94
Mark Pollmann <markpollmann@web.de>
2020-01-07 11:38:38 +0100

    add login screen e2e tests

--- cypress/integration/login.ts
+++ cypress/integration/login.ts
@@ -1,11 +1,39 @@
-describe('Login', () => {
+const username = 'testdrive@react-trello.now.sh'
+const password = 'testdrive'
+
+describe('Login Page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('/')
   })
 
-  it.skip('can login', () => {
+  it('Click on `Get Started` should route to /login', () => {
+    cy.location('pathname').should('be', '/')
+    cy.contains('Get Started').click()
+    cy.location('pathname').should('be', '/login')
+  })
+
+  it('Should login via login button', () => {
     cy.contains('Log in').click()
-    cy.contains('Submit').click()
-    cy.get('body').should('contain', 'Welcome')
+
+    cy.get('input[type="email"]').type(username)
+    cy.get('input[type="password"]').type(password)
+    cy.get('[type="submit"]').click()
+
+    cy.location('pathname').should('be', '/app')
+  })
+
+  // not login with false credentials
+  it('Should not login with wrong credentials', () => {
+    const falsePassword = 'asdf'
+
+    cy.visit('/app')
+    cy.location('pathname').should('be', '/')
+    cy.contains('Log in').click()
+
+    cy.get('input[type="email"]').type(username)
+    cy.get('input[type="password"]').type(falsePassword)
+    cy.get('[type="submit"]').click()
+
+    cy.location('pathname').should('not.be', '/app')
   })
 })
